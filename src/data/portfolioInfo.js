2da6db66575105f6ad60259ba0dbbf0fe36b26e7
Meissa Babou <meissababou66@gmail.com>
2026-05-13 18:52:09 +0000

    Reinitialisation

--- src/data/portfolioInfo.js
+++
@@ -1,9 +0,0 @@
-const portfolioInfo = {
-  fullName: "Meissa Babou",
-  title: "Etudiant en reseau et telecom",
-  specialization: "Junior IT administrateur systeme reseau et securite",
-  description:
-    "API REST de gestion de portfolio pour presenter et administrer les projets de Meissa Babou."
-};
-
-module.exports = portfolioInfo;
